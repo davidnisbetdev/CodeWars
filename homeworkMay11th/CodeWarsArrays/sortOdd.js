ed039aed01c4d4df1523709f27a02139b4a1a0f3
David Nisbet <nizz3hx@protonmail.com>
2021-05-13 21:48:05 +0100

    homework

---
+++ homeworkMay11th/CodeWarsArrays/sortOdd.js
@@ -0,0 +1,27 @@
+// Task
+
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+// Examples
+
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+
+function sortArray(array) {
+    // Return a sorted array.
+  const even = array.map((num) => num % 2 === 0 ? num : "");
+  const odd = array.filter((num) => num % 2 !== 0).sort((a, b) => a - b);
+  const result = [];
+  
+  for (let i = 0, j = 0; i < array.length; i += 1) {
+    if (even[i] === "") {
+      result.push(odd[+j]);
+      j += 1;
+    } else {
+      result.push(even[i]);
+    }
+  }
+  
+  return result;
+}
